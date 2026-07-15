2a7121c8b4f45d4a767205c285c7ccd738defeb2
jonas17esc <duartesilvjonashenry@gmail.com>
2026-07-15 10:46:34 -0300

    Commit das variavaeis exercicio 2

---
+++ python/AULA01/variaveis.py
@@ -0,0 +1,15 @@
+nome = "Jonas Henrique"
+profissao = "Estudante de programação"
+idade = 18 
+peso = "Não sei"
+altura = 1.70
+estudante = True
+linguagem = "Python"
+objetivo = "Ser um programador de qualidade e respeito"
+
+print("Nome :", nome)
+print("Profissao :", profissao)
+print("Idade :", idade)
+print("Peso :", peso)
+print("Altura :", altura)
+print("Estudante:", estudante)
