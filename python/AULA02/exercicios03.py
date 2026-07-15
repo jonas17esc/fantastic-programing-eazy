ee663f9933bb317f4db4bdbfe9b3e93c0bfa6d85
jonas17esc <duartesilvjonashenry@gmail.com>
2026-07-15 13:48:10 -0300

    Commit com as novas alterações

---
+++ python/AULA02/exercicios03.py
@@ -0,0 +1,16 @@
+# ===========================================
+# Curso de Python
+# Aula 01
+# Exercício 03
+# Tema: Entrada de Dados
+# Autor: Jonas Henrique
+# Data: 15/07/2026
+# ===========================================
+
+nome = input("Qual o seu nome ?  " )
+idade = int(input("Qual é a sua idade ? " ))
+cidade = input("Qual é a sua cidade ? " )
+
+print(f"Olá meu caro {nome}! seja bem vindo ao curso de python")
+print(f"Sua idade é {idade}, meus parabéns!")
+print(f"Sua cidade é {cidade}")
---
+++ python/AULA03/exercicio04.py
@@ -0,0 +1,9 @@
+# ===========================================
+# Curso de Python
+# Aula 03
+# Exercício 04
+# Tema: Operadores Matemáticos
+# Autor: Jonas Henrique
+# Data: 15/07/2026
+# ===========================================
+
