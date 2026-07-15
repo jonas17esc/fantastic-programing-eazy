5968716cfbda473a378106ae852645b97f674cb6
jonas17esc <duartesilvjonashenry@gmail.com>
2026-07-15 11:11:55 -0300

    Commit entrada exercicio 03

---
+++ python/AULA01/exercicios03.py
@@ -0,0 +1,10 @@
+#EXECICIO3
+# Entrada de dados em python
+
+nome = input("Qual o seu nome ?  " )
+idade = int(input("Qual é a sua idade ? " ))
+cidade = input("Qual é a sua cidade ? " )
+
+print(f"Olá meu caro {nome}! seja bem vindo ao curso de python")
+print(f"Sua idade é {idade}, meus parabéns!")
+print(f"Sua cidade é {cidade}")
