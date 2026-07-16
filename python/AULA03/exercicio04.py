e1c71c8390da8e4c85786f2a387c2890c0876944
jonas17esc <duartesilvjonashenry@gmail.com>
2026-07-16 16:57:09 -0300

    Git com o exercicio 5

--- python/AULA03/exercicio04.py
+++ python/AULA03/exercicio04.py
@@ -7,3 +7,26 @@
 # Data: 15/07/2026
 # ===========================================
 
+n1 = int(input("Digite um valor para o numero1 : "))
+n2 = int(input("Digite um valor para o numero2 : "))
+
+
+soma = n1 + n2
+subtracao = n1 - n2
+multiplicacao = n1 * n2
+divisao = n1 / n2
+divisao_exata = n1 // n2
+resto_divisao = n1 % n2
+potencia = n1 ** n2
+
+print(f"Soma: {soma}")
+print(f"Subtracao: {subtracao}")
+print(f"Multiplicacao: {multiplicacao}")
+print(f"Divisao: {divisao}")
+print(f"Divisao_exata: {divisao_exata}")
+print(f"Resto_divisao: {resto_divisao}")
+print(f"Potencia: {potencia}")
+
+
+
+
